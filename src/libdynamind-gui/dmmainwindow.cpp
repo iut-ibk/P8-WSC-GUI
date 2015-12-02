eb0a8e98dcca68eb052c777a1b469ad9adc8cdf4
Clemens Degasper <clemens.degasper@gmail.com>
2015-12-02 15:29:36 +0100

    new version nr

--- src/libdynamind-gui/dmmainwindow.cpp
+++ src/libdynamind-gui/dmmainwindow.cpp
@@ -981,7 +981,7 @@ void DMMainWindow::showAbout()
     QMessageBox msgBox;
     msgBox.setTextFormat(Qt::RichText);
     msgBox.setText(QString("<h4>WSC Modelling Toolkit</h4>\n\n"
-               "Version: 04.11.2015\n"
+               "Version: 02.12.2015\n"
                /*"<a href=\"http://www.such-and-such.com\">http://www.such-and-such.com</a>"*/));
     msgBox.setIconPixmap(QPixmap(":/Icons/ressources/P8-Tool-Logo_small.png"));
     msgBox.exec();
